a555dc828dca15f8c94734c878c67fffadd7477f
Alexander Dadiani <alexdadiani1994@gmail.com>
2024-05-27 21:29:27 +0200

    naive impl done

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,29 +1,44 @@
+use regex::Regex;
 use std::env;
 use std::fs;
 use std::process;
 
-static FILE_PATH: &str = "-file_path";
+const FILE_PATH: &str = "-file_path";
+const MATCH_EXPRESSION: &str = "-expression";
 
 #[derive(Debug, PartialEq)]
 struct Args {
     fp: String,
+    me: String,
 }
 
 impl Args {
     fn new(raw: Vec<String>) -> Args {
-        let mut args = Args { fp: String::new() };
+        let mut args = Args {
+            fp: String::new(),
+            me: String::new(),
+        };
 
         let mut arg_iter = raw.iter();
         loop {
-            let x = arg_iter.next();
-            match x {
+            let maybe_arg = arg_iter.next();
+            match maybe_arg {
                 None => return args,
-                Some(x) => {
-                    if x == FILE_PATH {
+                Some(arg) => match arg.as_str() {
+                    FILE_PATH => {
                         let v = arg_iter.next();
                         args.fp = v.unwrap_or(&String::new()).clone();
                     }
-                }
+
+                    MATCH_EXPRESSION => {
+                        let v = arg_iter.next();
+                        args.me = v.unwrap_or(&String::new()).clone();
+                    }
+
+                    _ => {
+                        continue;
+                    }
+                },
             }
         }
     }
@@ -32,8 +47,6 @@ impl Args {
 fn main() {
     let args_raw: Vec<String> = env::args().collect();
 
-    let args_raw = dbg!(args_raw);
-
     let args = Args::new(args_raw);
 
     if args.fp == String::new() {
@@ -41,7 +54,10 @@ fn main() {
         process::exit(1);
     }
 
-    let args = dbg!(args);
+    if args.me == String::new() {
+        println!("No regex given.");
+        process::exit(1);
+    }
 
     let file_contents = match fs::read_to_string(&args.fp) {
         Ok(file) => file,
@@ -51,7 +67,20 @@ fn main() {
         }
     };
 
-    let file_contents = dbg!(file_contents);
+    let regex_parsed = match Regex::new(&args.me) {
+        Ok(rg) => rg,
+        Err(err) => {
+            println!("Could not process expression {}: {}", &args.me, err);
+            process::exit(1);
+        }
+    };
+
+    let found: Vec<&str> = regex_parsed
+        .find_iter(&file_contents)
+        .map(|m| m.as_str())
+        .collect();
+
+    dbg!(found);
 }
 
 #[cfg(test)]
@@ -60,35 +89,26 @@ mod tests {
 
     #[test]
     fn new_args() {
+        let default_expression = String::from("*");
         let file_path = String::from("./path/to/file.txt");
         let correct_input: Vec<String> = vec![
             String::from("target/debug/minigrep"),
-            String::from("-file_path"),
+            String::from(FILE_PATH),
             file_path.clone(),
+            String::from(MATCH_EXPRESSION),
+            default_expression.clone(),
         ];
         let args = Args::new(correct_input);
         let expected_args = Args {
             fp: file_path.clone(),
+            me: default_expression.clone(),
         };
 
         assert_eq!(args, expected_args);
 
-        let missing_arg_input: Vec<String> = vec![
-            String::from("target/debug/minigrep"),
-            String::from("-file_path"),
-        ];
-        let args = Args::new(missing_arg_input);
-        let expected_args = Args { fp: String::new() };
-
-        assert_eq!(args, expected_args);
-
-        let no_arg_input: Vec<String> = vec![];
-        let args = Args::new(no_arg_input);
-        let expected_args = Args { fp: String::new() };
-
-        assert_eq!(args, expected_args);
-
         let different_order_arg_input: Vec<String> = vec![
+            String::from(MATCH_EXPRESSION),
+            default_expression.clone(),
             String::from("something else"),
             String::from("-file_path"),
             file_path.clone(),
@@ -97,6 +117,30 @@ mod tests {
         let args = Args::new(different_order_arg_input);
         let expected_args = Args {
             fp: file_path.clone(),
+            me: default_expression.clone(),
+        };
+
+        // Err cases.
+
+        assert_eq!(args, expected_args);
+
+        let missing_arg_input: Vec<String> = vec![
+            String::from("target/debug/minigrep"),
+            String::from("-file_path"),
+        ];
+        let args = Args::new(missing_arg_input);
+        let expected_args = Args {
+            fp: String::new(),
+            me: String::new(),
+        };
+
+        assert_eq!(args, expected_args);
+
+        let no_arg_input: Vec<String> = vec![];
+        let args = Args::new(no_arg_input);
+        let expected_args = Args {
+            fp: String::new(),
+            me: String::new(),
         };
 
         assert_eq!(args, expected_args);
