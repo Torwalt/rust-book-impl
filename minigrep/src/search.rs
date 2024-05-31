2c8470d37dacd99331e7f074f364d0e1b00fc1f6
Alexander Dadiani <alexdadiani1994@gmail.com>
2024-05-31 20:54:34 +0200

    minigrep: make search more functional

--- minigrep/src/search.rs
+++ minigrep/src/search.rs
@@ -1,15 +1,5 @@
-pub fn lines_containing_search(search: &String, text: &String) -> Vec<String> {
-    let mut out: Vec<String> = Vec::new();
-
-    for line in text.lines() {
-        if !line.contains(search) {
-            continue;
-        }
-
-        out.push(String::from(line))
-    }
-
-    return out;
+pub fn lines_containing_search<'a>(search: &str, text: &'a str) -> Vec<&'a str> {
+    return text.lines().filter(|line| line.contains(search)).collect();
 }
 
 #[cfg(test)]
@@ -17,9 +7,8 @@ mod tests {
     use super::*;
     #[test]
     fn test_lines_containing_search() {
-        let search = String::from("Lorem");
-        let text = String::from(
-            "
+        let search = "Lorem";
+        let text = "
 Lorem ipsum dolor sit amet,
 consetetur sadipscing elitr,
 sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
@@ -27,13 +16,12 @@ sed diam voluptua Lorem.
 At vero eos et accusam et justo duo dolores et ea rebum.
 Stet clita kasd gubergren,
 no sea takimata sanctus est Lorem ipsum dolor sit amet.
-",
-        );
+";
 
         let exp = vec![
-            String::from("Lorem ipsum dolor sit amet,"),
-            String::from("sed diam voluptua Lorem."),
-            String::from("no sea takimata sanctus est Lorem ipsum dolor sit amet."),
+            "Lorem ipsum dolor sit amet,",
+            "sed diam voluptua Lorem.",
+            "no sea takimata sanctus est Lorem ipsum dolor sit amet.",
         ];
         let result = lines_containing_search(&search, &text);
         assert_eq!(exp, result);
